d6633dcb559d6a48782d53e7dd51085dc1eafba1
henriqueAlgauer <algauer.henrique@gmail.com>
2024-12-09 23:33:04 -0300

    final commit

--- cmd/main.go
+++ cmd/main.go
@@ -106,6 +106,9 @@ func main() {
 	page := newPage()
 	e.Renderer = newTemplate()
 
+	e.Static("/images", "images")
+	e.Static("/css", "css")
+
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "index", page)
 	})
@@ -149,72 +152,3 @@ func main() {
 
 	e.Logger.Fatal(e.Start((":42069")))
 }
-
-// package main
-
-// import (
-// 	"html/template"
-// 	"io"
-// 	"net/http"
-// 	"strconv"
-
-// 	"github.com/labstack/echo/v4"
-// 	"github.com/labstack/echo/v4/middleware"
-// )
-
-// type Templates struct {
-// 	templates *template.Template
-// }
-
-// func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-// 	return t.templates.ExecuteTemplate(w, name, data)
-// }
-
-// func NewTemplates() *Templates {
-// 	return &Templates{
-// 		templates: template.Must(template.ParseGlob("views/*.html")),
-// 	}
-// }
-
-// type Block struct {
-//     Id int
-// }
-
-// type Blocks struct {
-//     Start int
-//     Next int
-//     More bool
-//     Blocks []Block
-// }
-
-// func main() {
-// 	e := echo.New()
-//     e.Renderer = NewTemplates()
-//     e.Use(middleware.Logger())
-
-//     e.GET("/blocks", func(c echo.Context) error {
-//         startStr := c.QueryParam("start")
-//         start, err := strconv.Atoi(startStr)
-//         if err != nil {
-//             start = 0
-//         }
-
-//         blocks := []Block{}
-//         for i := start; i < start + 10; i++ {
-//             blocks = append(blocks, Block{Id: i})
-//         }
-
-//         template := "blocks"
-//         if start == 0 {
-//             template = "blocks-index"
-//         }
-//         return c.Render(http.StatusOK, template, Blocks{
-//             Start: start,
-//             Next: start + 10,
-//             More: start + 10 < 100,
-//             Blocks: blocks,
-//         });
-//     });
-
-//     e.Logger.Fatal(e.Start(":42069"))
-// }
